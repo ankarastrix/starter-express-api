e4e47a6e49d34175a7dd025448fcb0f97725c6d7
ankarastrix <85880548+ankarastrix@users.noreply.github.com>
2023-10-25 15:12:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+
+const app = express();
+
+// Konfigurišite proxy za POST rutu '/'
+app.post('/',(req,res)=> res.send({'post':'ok'}));
+
+app.post('/:id', (req, res, next) => {
+  const id = req.params.id; // Dobijanje ID parametra iz URL-a
+  const targetUrl = `https://api.klix.ba/v1/rate/${id}`; // Konstruisanje ciljne adrese sa ID
+  
+  res.redirect(308, targetUrl); 
+   // Pozivamo proxy kako bismo prosledili zahtev ciljnoj adresi
+});
+
+// Ostatak vaših Express ruta i podešavanja
+
+app.listen(3000, () => {
+  console.log('Server je pokrenut na portu 3000');
+});
